290950f05f764565c5160ec09928a48e03affc15
MarkZither <mark.burton@zither-it.co.uk>
2023-02-27 16:15:28 +0100

    Add test setup for mocha/nyc

---
+++ tests/view.test.js
@@ -0,0 +1,12 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var assert = require('assert');
+    describe('Array', function () {
+        describe('#indexOf()', function () {
+            it('should return -1 when the value is not present', function () {
+                assert.equal([1, 2, 3].indexOf(4), -1);
+            });
+        });
+    });
+});
